a80f6f30b44518770ac2e5cd79deea06155091d0
Manisha Kumari <manisha.tkg08@gmail.com>
2024-12-16 10:36:53 +0530

    correted the file name and add a random barGraph-Project

--- chapter-0/randomWalker/randomWalker.js
+++ chapter-0/randomWalker/randomWalker.js
@@ -1,25 +1,33 @@
-// An array to keep track of how often random numbers are picked
-let randomCounts = [];
-// The total number of counts
-let total = 20;
+let walker;
 
 function setup() {
   createCanvas(640, 240);
-  for (let i = 0; i < total; i++) {
-    randomCounts[i] = 0;
-  }
+  walker = new Walker();
+  background(255);
 }
 
 function draw() {
-  background(255);
-  //{!2} Pick a random number and increase the count.
-  let index = floor(random(randomCounts.length));
-  randomCounts[index]++;
-  stroke(0);
-  fill(127);
-  let w = width / randomCounts.length;
-  // Graph the results.
-  for (let x = 0; x < randomCounts.length; x++) {
-    rect(x * w, height - randomCounts[x], w - 1, randomCounts[x]);
+  walker.step();
+  walker.show();
+}
+
+class Walker {
+  constructor() {
+    this.x = width / 2; // Starting position (center)
+    this.y = height / 2;
+  }
+
+  show() {
+    stroke(0);
+    point(this.x, this.y); // Draw object
+  }
+
+  step() {
+    // Random movement with equal probability
+    let choice = floor(random(4));
+    if (choice === 0) this.x++; // Right
+    else if (choice === 1) this.x--; // Left
+    else if (choice === 2) this.y++; // Down
+    else this.y--; // Up
   }
 }
